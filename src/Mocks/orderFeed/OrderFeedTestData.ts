54f60d1e0ee591a5748ab601a5f213afd9632e1b
Artem <artmzvn@gmail.com>
2021-09-25 13:39:07 +0300

    Order feed fix

--- src/Mocks/orderFeed/OrderFeedTestData.ts
+++ src/Mocks/orderFeed/OrderFeedTestData.ts
@@ -3,6 +3,7 @@ import { OrderType } from '@/Models/Enums';
 
 export const orderFeedTestData: OrderFeedModel[] = [
   {
+    id: 0,
     order_id: 654116,
     date: '2021-05-12',
     barcode: '15726283',
@@ -17,6 +18,7 @@ export const orderFeedTestData: OrderFeedModel[] = [
     nm_id: 2211113,
   },
   {
+    id: 1,
     order_id: 654119,
     date: '2021-05-12',
     barcode: '15726283',
@@ -31,6 +33,7 @@ export const orderFeedTestData: OrderFeedModel[] = [
     nm_id: 2245113,
   },
   {
+    id: 2,
     order_id: 654170,
     date: '2021-05-12',
     barcode: '15726283',
@@ -45,6 +48,7 @@ export const orderFeedTestData: OrderFeedModel[] = [
     nm_id: 2221113,
   },
   {
+    id: 3,
     order_id: 654180,
     date: '2021-05-12',
     barcode: '15726283',
@@ -59,6 +63,7 @@ export const orderFeedTestData: OrderFeedModel[] = [
     nm_id: 2216432,
   },
   {
+    id: 4,
     order_id: 654181,
     date: '2021-05-12',
     barcode: '15726283',
